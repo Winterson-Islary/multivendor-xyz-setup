894ab6179d3de7f561f8192a931d3c8763bb1ba5
Winterson <u19cse1028@cit.ac.in>
2025-11-07 20:35:28 +0530

    modify: add cors setup to the api route file

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,8 +1,25 @@
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 import { handle } from "hono/vercel";
+import { appConfig } from "@/lib/env.config";
 import { registerRoutes } from "@/server/routes";
 
 const app = new Hono().basePath("/api");
+app.use(
+	cors({
+		origin: appConfig.next_url,
+		allowHeaders: [
+			"X-Custom-Header",
+			"Upgrade-Insecure-Requests",
+			"Content-Type",
+			"Authorization",
+		],
+		allowMethods: ["POST", "GET", "PUT", "DELETE", "OPTIONS", "PATCH"],
+		exposeHeaders: ["Content-Length", "X-Kuma-Revision"],
+		maxAge: 600,
+		credentials: true,
+	}),
+);
 
 registerRoutes(app);
 
