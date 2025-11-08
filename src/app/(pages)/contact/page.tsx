46c30cd98805b0e2a9003d7fc908beec3024f3e6
Winterson <u19cse1028@cit.ac.in>
2025-11-08 21:46:09 +0530

    add contact and store pages

---
+++ src/app/(pages)/contact/page.tsx
@@ -0,0 +1,9 @@
+import Contact from "@/components/contact";
+
+export default function ContactPage() {
+	return (
+		<article className="jusitfy-center flex h-full w-full items-center">
+			<Contact />
+		</article>
+	);
+}
