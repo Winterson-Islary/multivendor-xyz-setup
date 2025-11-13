9f61a0d276e079a9498f360d45d582f67b8d28a4
Winterson <u19cse1028@cit.ac.in>
2025-11-13 22:48:43 +0530

    modify: add env variables to env.config and format utils.ts

--- src/lib/env.config.ts
+++ src/lib/env.config.ts
@@ -1,10 +1,18 @@
 import z from "zod";
+import "dotenv/config";
 
 const configSchema = z.object({
 	next_url: z.url().default("http://localhost:3000"),
 	server: z.object({
 		node_env: z.string().default("development"),
 		log_level: z.string().default("info"),
+	}),
+	auth: z.object({
+		secret: z.string(),
+		url: z.url(),
+	}),
+	db: z.object({
+		url: z.string().default("HErrrro"),
 	}),
 });
 
@@ -15,6 +23,13 @@ const loadConfig = () => {
 			node_env: process.env.NODE_ENV,
 			log_level: process.env.LOG_LEVEL,
 		},
+		auth: {
+			secret: process.env.BETTER_AUTH_SECRET,
+			url: process.env.BETTER_AUTH_URL,
+		},
+		db: {
+			url: process.env.DATABASE_URL,
+		},
 	};
 
 	try {
@@ -23,8 +38,8 @@ const loadConfig = () => {
 		if (error instanceof z.ZodError) {
 			console.error(
 				"❌ Invalid environment configuration:",
-				// error.flatten().fieldErrors,
-				z.treeifyError(error),
+				error.flatten().fieldErrors,
+				// z.treeifyError(error),
 			);
 			throw new Error(
 				"Invalid environment variables. Check the console for details.",
@@ -33,6 +48,5 @@ const loadConfig = () => {
 		throw error;
 	}
 };
-
 export const appConfig = Object.freeze(loadConfig());
 export type AppConfig = z.infer<typeof configSchema>;
