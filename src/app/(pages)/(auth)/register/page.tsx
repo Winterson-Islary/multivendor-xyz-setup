773e78f6654ca3dd2d8bce20c953e3426e213194
Winterson <u19cse1028@cit.ac.in>
2025-11-14 18:16:43 +0530

    create register user page

---
+++ src/app/(pages)/(auth)/register/page.tsx
@@ -0,0 +1,9 @@
+import { SignupForm } from "@/components/features/auth/signup-form";
+
+export default function RegisterPage() {
+	return (
+		<article className="flex h-full w-full items-center justify-center">
+			<SignupForm />
+		</article>
+	);
+}
